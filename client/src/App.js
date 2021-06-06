752621252b8e09358ab50636845f3407f69864b0
Stephanie Ann Solis <stephaniesolis@lambdastudents.com>
2021-06-06 17:02:39 -0600

    tried but could not render movie cards

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+//SAS - importing dependencies
+import { Route } from 'react-router-dom';
+
+//SAS - component imports
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 export default function App () {
-  const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
+  // const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
   const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
@@ -14,6 +20,10 @@ export default function App () {
         .then(response => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+
+          //SAS - console successfully showed data, setMovieList gives movieList new data.
+          //console.log(response.data)
+          setMovieList(response.data);
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -21,16 +31,23 @@ export default function App () {
     }
     getMovies();
   }, []);
+  
+  //SAS - data successfully passed into state
+  //console.log(movieList)
 
-  const addToSavedList = id => {
-    // This is stretch. Prevent the same movie from being "saved" more than once
-  };
+  // const addToSavedList = id => {
+  //   // This is stretch. Prevent the same movie from being "saved" more than once
+  // };
 
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <div>
+        <Route exact path='/' render={() => <MovieList movies={movieList} />} />
+
+        <Route path='/movies/:id'><Movie /></Route> 
+      </div>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+// SAS- Importing Router
+import {BrowserRouter as Router} from 'react-router-dom';
 
 // You'll need to wrap <App /> for routing to work
-ReactDOM.render(<App />, document.getElementById('root'));
+// SAS - Browser is wrapping App
+ReactDOM.render(
+<Router>
+    <App />
+</Router>, document.getElementById('root')
+);
